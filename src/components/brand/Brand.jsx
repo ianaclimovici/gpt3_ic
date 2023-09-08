838263f2754e1a5db7aa82dc32f51e2505e0fee5
ianaclimovici <97248558+ianaclimovici@users.noreply.github.com>
2023-09-08 20:08:43 +0200

    Brand and Feature

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import {google, slack, atlassian, shopify, dropbox} from './imports'
 
 import './brand.css'
 
 const Brand = () => {
   return (
-    <div>Brand</div>
+    <div className='gpt3__brand section__padding'>
+      <div>
+        <img src={google} alt="google" />
+      </div>
+      <div>
+        <img src={slack} alt="google" />
+      </div>
+       <div>
+        <img src={atlassian} alt="google" />
+      </div>
+      <div>
+        <img src={dropbox} alt="google" />
+      </div>
+      <div>
+        <img src={shopify} alt="google" />
+      </div>
+    </div>
   )
 }
 
